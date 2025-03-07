ddc59f230b801f6cacf111750bb9b32090c6c3eb
Lucas OMS <lucas.oms@hotmail.fr>
2025-03-07 07:37:09 +0100

    feat: change way to get parser to support ESLint 9 configs

--- src/rules/html-attributes-order.ts
+++ src/rules/html-attributes-order.ts
@@ -50,17 +50,21 @@ const rule: Rule.RuleModule = {
     },
     create: context => {
         // Check which parser is being used
-        const parser = context.parserPath ?? '';
+        const parser =
+            context.parserPath // ESLint 8 way to get it
+            ?? context.languageOptions?.parser?.meta?.name; // ESLint 9 way to get it
 
-        if (parser.includes('@angular-eslint')) {       // @angular-eslint/template-parser
+        if (!parser) {
+            throw new Error('Parser is undefined');
+        }
+
+        if (parser.includes('angular-eslint')) {       // @angular-eslint/template-parser
             return htmlAttributesOrderRuleForAngularTemplateParser(context);
-        } else if (parser.includes('@html-eslint')) {   // @html-eslint/parser
+        } else if (parser.includes('html-eslint')) {   // @html-eslint/parser
             // Logique spécifique au parser HTML
             return htmlAttributesOrderRuleForHtmlParser(context);
-        } else {
-            // If the parser is not supported, return an empty object to avoid any errors, no rule will be applied
-            return {};
         }
+        throw new Error(`Unsupported parser, please use @angular-eslint/template-parser or @html-eslint/parser, current is ${parser}`);
     },
 };
 
